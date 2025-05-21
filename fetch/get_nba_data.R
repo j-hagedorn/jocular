e420011b8ab440b3bc48aa826d82c54c892f31ab
Joshh <j-hagedorn@users.noreply.github.com>
2025-05-21 17:44:28 -0400

    Update files

--- fetch/get_nba_data.R
+++ fetch/get_nba_data.R
@@ -3,17 +3,20 @@ library(nbastatR);library(tidyverse)
 # https://github.com/rajshah4/BasketballData
 # http://asbcllc.com/nbastatR/index.html
 
-Sys.setenv("VROOM_CONNECTION_SIZE" = 131072 * 2)
+Sys.setenv("VROOM_CONNECTION_SIZE" = 131072 * 3)
 
 games <-
   game_logs(
-    seasons = 1959:2023, league = "NBA", result_types = "player",
+    seasons = 1959:2024, league = "NBA", result_types = "player",
     season_types = "Regular Season", nest_data = F,
     assign_to_environment = TRUE, return_message = TRUE
   )
 
-player_season <- bref_players_stats(seasons = 1978:2022, tables = c("advanced", "totals", "per_game"))
-drafts <- drafts(draft_years = 2023, nest_data = F, return_message = T)
+player_season <- arrow::read_parquet("data/player_season.parquet")
+new_season <- bref_players_stats(seasons = 2024, tables = c("advanced","per_game"))
+player_season <- player_season %>% bind_rows(new_season)
+  
+drafts <- drafts(draft_years = 2024, nest_data = F, return_message = T)
 
 arrow::write_parquet(games,"data/games.parquet")
 arrow::write_parquet(player_season,"data/player_season.parquet")
